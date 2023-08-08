e979c73055fad1f59d5e87ff203e7606ca35fae9
[Seungmin-github] <[dltmdals2345@naver.com]>
2023-08-08 09:38:29 +0900

    first commit

---
+++ src/main/java/com/multicampus/sample/mapper/TimeMapper.java
@@ -0,0 +1,9 @@
+package com.multicampus.sample.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+
+    @Select("select now()")
+    String getTime();
+}
---
+++ src/main/java/com/multicampus/sample/sample/SampleDAO.java
@@ -0,0 +1,4 @@
+package com.multicampus.sample.sample;
+
+public interface SampleDAO {
+}
